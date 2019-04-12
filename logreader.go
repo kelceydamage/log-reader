5c84574c794ef8bdeab230f1125f770f8ddc3588
kelceydamage <kelcey@kelceydamage.com>
2019-04-12 15:13:15 -0700

    convert to spaces

--- logreader.go
+++ logreader.go
@@ -1,10 +1,10 @@
 // Package main is a server for emitting new log lines over a socket.
 package main
-	
+    
 import (
-	"fmt"
-	"net"
-	"os"
+    "fmt"
+    "net"
+    "os"
 
     "github.com/hpcloud/tail"
 )
@@ -16,11 +16,11 @@ const (
 )
 
 var (
-	ioconfig = tail.Config{
-		Follow: true,
-		Poll: false,
-		ReOpen: true,
-	}
+    ioconfig = tail.Config{
+        Follow: true,
+        Poll: false,
+        ReOpen: true,
+    }
 )
 
 func check(e error) {
@@ -30,11 +30,11 @@ func check(e error) {
 }
 
 func main() {
-	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
-	check(err)
-	
-	defer l.Close()
-	for {
+    l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+    check(err)
+    
+    defer l.Close()
+    for {
         conn, err := l.Accept()
         if err != nil {
             fmt.Println("Error accepting: ", err.Error())
@@ -42,7 +42,7 @@ func main() {
         }
         go handleRequest(conn)
     }
-	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
+    fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
 }
 
 func handleRequest(conn net.Conn) {
@@ -51,8 +51,8 @@ func handleRequest(conn net.Conn) {
 
   // Send the lines from the log and stay open for new lines.
   for line := range t.Lines {
-  	fmt.Println(line.Text)
-  	conn.Write([]byte(line.Text))
+    fmt.Println(line.Text)
+    conn.Write([]byte(line.Text))
   }
 
   conn.Close()
